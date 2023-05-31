bda3cbf993395a5c657ec8e597a27be89b5a0b6e
puppet-coder <123579247+puppet-coder@users.noreply.github.com>
2023-05-31 10:12:36 +0530

    3rd commit

--- test/SagoToken.test.js
+++ test/SagoToken.test.js
@@ -1,28 +1,55 @@
 const SagoToken = artifacts.require("SagoToken");
 const BN = require("bn.js");
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:8545');
+
 contract("SagoToken", (accounts) => {
-    it('should credit an NFT to a specific account', async () => {
+    // Buy 
+    it('Token Bought successfully', async () => {
         const SagoTokenInstance = await SagoToken.deployed();
-
-        // Execute the transaction that emits the event
-        const tx = await SagoTokenInstance.BuyToken({ value: 1000000000000000000 });
-
-        // Retrieve the emitted events
-        const events = tx.receipt.logs;
-
-        // Access the first emitted event
-        const event = events[0];
-
-        // Access event properties
-        const eventName = event.event;
-        const args = event.args;
+        const tx = await SagoTokenInstance.BuyToken({ from: accounts[0], value: 1000000000000000000 });
         console.log("Amounts: " + new BN(tx.logs[1].args.amount));
         console.log("Buyer: " + tx.logs[1].args.Buyer);
         console.log("NoofToken: " + new BN(tx.logs[1].args.NoofToken));
         console.log("totalBalance: " + new BN(tx.logs[1].args.totalBalance));
         // Perform assertions or other actions with the event data
-        assert.equal(tx.logs[1].args.Buyer, accounts[0], "Buyer successfully");
-        assert.equal(new BN(tx.logs[1].args.NoofToken)>0, true, "Buyer successfully");
+        assert.equal(new BN(tx.logs[1].args.NoofToken)>0, true, "Token Bought successfully");
+
+    });
+
+    //Check the balance
+    it('Should view balance', async () => {
+        const SagoTokenInstance = await SagoToken.deployed();
+
+        // Execute the transaction that emits the event
+        const tx = await SagoTokenInstance.getWalletBalance();
+
+        console.log("Amounts: " + new BN(tx));
+        assert.equal(new BN(tx)>0, true, "Token Bought successfully");
+
+    });
+
+    //Sell Token
+    it('should sell tokens ', async () => {
+        const SagoTokenInstance = await SagoToken.deployed();
+
+        const tokenToSell = 1; // Adjust the value as needed
+        
+        await SagoTokenInstance.BuyToken({ from: accounts[0], value: 1000000000000000000 });
+        const tx = await SagoTokenInstance.SellToken(tokenToSell, { from:accounts[0]});
+        console.log("********************From amount transfer********************")
+        console.log(tx);
+        assert.equal(tx.receipt.status, true, "Token Bought successfully");
+
+    });
+
+    //Transfer tokens
+    it('should transfer the tokens to someone address ', async () => {
+        const SagoTokenInstance = await SagoToken.deployed();
+        const tokenToSell = 1; // Adjust the value as needed
+        await SagoTokenInstance.BuyToken({ from: accounts[0], value: 1000000000000000000 });
+        const tx = await SagoTokenInstance.TranferToken(accounts[1],tokenToSell, { from:accounts[0]});
+        assert.equal(tx.receipt.status, true, "Token transferred successfully");
 
     });
 });
